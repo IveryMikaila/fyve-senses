e3e4ee81c85f22f123cd70bfd872a01965986a58
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2023-10-23 13:19:41 -0400

    Added Font assets

--- portfolio/src/Components/Home.js
+++ portfolio/src/Components/Home.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import "../Styles/Home.css"
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='homePage'>
+      {/* Page Intro Animation */}
+<div className='pageIntro'>
+<h1 className='logo-header'>
+<span className='logo'>Fyve</span> <span className='logo'>Senses</span>
+</h1>
+</div>
+
+
+    </div>
   )
 }
 
