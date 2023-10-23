cc5221b1968252db1fada2a639d7e20486d26a14
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2023-10-23 15:01:56 -0400

    Coded Homepage animation splash screen

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../src/Styles/Base.css";
 import { BrowserRouter,Route,Routes } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
--- portfolio/src/Components/Home.js
+++ portfolio/src/Components/Home.js
@@ -1,16 +1,42 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import "../Styles/Home.css"
 
 const Home = () => {
+
+function introFade(){
+  let intro = document.getElementById('pageIntro');
+  intro.style.opacity = "0.5"
+}
+function introFade2(){
+  let intro = document.getElementById('pageIntro');
+  intro.style.opacity = "0"
+}
+function introAnimation (){
+  let intro = document.getElementById('pageIntro');
+intro.style.display ="none"
+}
+
+useEffect(()=>{
+
+  setTimeout(introFade, 6005)
+  setTimeout(introFade2, 6010)
+  setTimeout(introAnimation, 7000)
+
+},[])
+
+
   return (
     <div className='homePage'>
       {/* Page Intro Animation */}
-<div className='pageIntro'>
-<h1 className='logo-header'>
+<div id='pageIntro' className='pageIntro'>
+<h1 id='logo-header' className='logo-header'>
 <span className='logo'>Fyve</span> <span className='logo'>Senses</span>
 </h1>
 </div>
 
+
+{/* Home Page */}
+<div>Home</div>
 
     </div>
   )
