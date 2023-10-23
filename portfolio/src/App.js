a1ca5d70a4478e0324cafd714722344fdfb46943
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2023-10-23 12:37:59 -0400

    Added core components
    Added page components
    Added page routes for each component

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,10 +1,25 @@
 import React from "react";
+import { BrowserRouter,Route,Routes } from "react-router-dom";
+import Home from "./Components/Home";
+import About from "./Components/About";
+import Services from "./Components/Services";
+import Work from "./Components/Work";
+import Contact from "./Components/Contact";
 
 
 function App() {
   return (
     <div className="App">
+<BrowserRouter>
+<Routes>
+<Route path="/" element={<Home />} />
+<Route path="/aboutus" element={<About />} />
+<Route path="/our-services" element={<Services />} />
+<Route path="/work" element={<Work />} />
+<Route path="/contact" element={<Contact />} />
 
+</Routes>
+</BrowserRouter>
     </div>
   );
 }
