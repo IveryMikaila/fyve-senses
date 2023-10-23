79534e55227a81ad3a156c4715991fdf6316eb18
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2023-10-23 12:42:37 -0400

    Added navbar and footer components

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -5,21 +5,24 @@ import About from "./Components/About";
 import Services from "./Components/Services";
 import Work from "./Components/Work";
 import Contact from "./Components/Contact";
+import Navbar from "./Components/Page-Components/Navbar";
+import Footer from "./Components/Page-Components/Footer";
 
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
 <BrowserRouter>
 <Routes>
 <Route path="/" element={<Home />} />
-<Route path="/aboutus" element={<About />} />
+<Route path="/about-us" element={<About />} />
 <Route path="/our-services" element={<Services />} />
 <Route path="/work" element={<Work />} />
 <Route path="/contact" element={<Contact />} />
-
 </Routes>
 </BrowserRouter>
+<Footer />
     </div>
   );
 }
