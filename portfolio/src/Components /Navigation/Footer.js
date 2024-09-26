fbb71de8a7c21a476a85717a1978320c402ea8c6
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2024-09-26 11:41:26 -0400

    Added Footer Links
    -Added Contact Information, Social Media Accounts, and other footer details.

--- portfolio/src/Components /Navigation/Footer.js
+++ portfolio/src/Components /Navigation/Footer.js
@@ -2,7 +2,50 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <>
+      <div className='footer-menu'>
+        <div>
+          <h3>Where Creativity is Engineered.</h3>
+        <a href='/contact'>Get in Touch &rarr;</a>
+        </div>
+
+<div className='footer-links'>
+<div>
+<h3>LEARN MORE</h3>
+<ul>
+  <li>
+    <a href='/services'>Our Services</a>
+  </li>
+  <li>
+    <a href='/work'>Work</a>
+  </li>
+</ul>
+</div>
+
+<div>
+<h3>FOLLOW US</h3>
+<ul>
+  <li>
+    <a href='https://tallerderafa.com/' target='_blank'>Taller De Rafa</a>
+  </li>
+  <li>
+    <a href='https://www.instagram.com/fyvesenses/' target='_blank'>Instagram</a>
+  </li>
+  <li>
+    <a href='' target='_balnk'>Linkedin</a>
+  </li>
+  <li>
+    <a href='mailto:Genesis@fyvesenses.com' target='_blank'>Email</a>
+  </li>
+</ul>
+</div>
+
+</div>
+      </div>
+<div className='footer-tagline'>
+<h1>It's About Ideas </h1>
+</div>
+    </>
   )
 }
 
