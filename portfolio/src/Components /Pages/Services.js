a9d5448fa22ec6bde85ce429509d27553efcd688
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2024-10-03 11:26:51 -0400

    Added Services Copy to Service Page

--- portfolio/src/Components /Pages/Services.js
+++ portfolio/src/Components /Pages/Services.js
@@ -1,8 +1,20 @@
 import React from 'react'
+import {service_data } from '../Data/Services_Data'
+import '../../Styles/Services.css'
 
 const Services = () => {
+
   return (
-    <div className='service-page'>Services</div>
+    <div>
+      <h1 className='service-h1'>Services</h1>
+{service_data.map((service)=>{
+  return(<>
+  <h2>{service.name}</h2>
+  <p>{service.description}</p>
+  </>)
+})}
+
+    </div>
   )
 }
 
