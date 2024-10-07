8d1eb5faa867f2894da7917c4abf241f2f36ed76
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2024-10-07 15:16:08 -0400

    Added/ Styled Contact Form
    Styled the Contact Form Page

--- portfolio/src/Components /Pages/Contact.js
+++ portfolio/src/Components /Pages/Contact.js
@@ -1,8 +1,23 @@
 import React from 'react'
+import '../../Styles/Contact.css'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div className='contact-page'>
+<h1>CONTACT</h1>
+<p>Interested in Working with us?<br/> Let's start from Scratch.</p>
+
+<div className='contact-form-container'>
+<form action='' method=''>
+<input type="text" name="fullname" required placeholder='Name' />
+<input type="email" name="email" required placeholder='Email' />
+<input type="tel" name="Phone" required placeholder='Phone' />
+<textarea cols={50} rows={4} required placeholder='Message'></textarea>
+<button className='contact-form-btn' type="submit"> Submit</button>
+</form>
+</div>
+
+    </div>
   )
 }
 
--- portfolio/src/Components /Pages/Services.js
+++ portfolio/src/Components /Pages/Services.js
@@ -6,7 +6,7 @@ const Services = () => {
 
   return (
     <div>
-      <h1 className='service-h1'>Services</h1>
+      <h1 className='service-h1'>SERVICES</h1>
 {service_data.map((service)=>{
   return(<div className={service.class}>
   <h2 className='service-h2'>{service.name}</h2>
