8834af604a2d58c878a503e5cd862157ce3e26a7
IveryMikaila <112774755+IveryMikaila@users.noreply.github.com>
2024-10-08 15:19:48 -0400

    Added Client Information to Work Page

---
+++ portfolio/src/Components /Data/Clients.js
@@ -0,0 +1,75 @@
+export let client_list = [
+
+{
+    name:`Jordan Brand All Star Week 2024`,
+    class:`client client-1`,
+    path:`/jordan-2024
+`
+},
+{
+    name:`Taller De Rafa Collection 001`,
+    class:`client client-2`,
+    path:`/tdr-collection-01`
+},
+
+{
+    name:`Spike Lee's Do The Right Thing 35th Anniversary`,
+    class:`client client-3`,
+    path:`/spike-lee-anniversary`
+},
+{
+    name:`Snapchat 2023`,
+    class:`client client-4`,
+    path:`/snapchat-2023`
+},
+{
+    name:`Nike World Basketball Festival 2023`,
+     class:`client client-5`,
+    path:`/nike-basketball-2023`
+},
+{
+    name:`Nike Hip Hop Friends and Family`,
+     class:`client client-6`,
+    path:`/nike-hip-hop`
+},
+{
+    name:`Nike Baseball Jackie Robinson Day`,
+    class:`client client-7`,
+    path:`/nike-baseball`
+},
+{
+    name:`Nike Air Max Day Workshop 2023`,
+     class:`client client-8`,
+    path:`/nike-max-workshop-2023`
+},
+{
+    name:`Nike Air Max Day Unite Stores`,
+     class:`client client-9`,
+    path:`/nike-max-unite`
+},
+{
+    name:`Met Gala Vogue`,
+     class:`client client-10`,
+    path:`/met-vogue`
+},
+{
+    name:`Instagram Dale Tu 2023`,
+     class:`client client-11`,
+    path:`/instagram-2023`
+},
+{
+    name:`Apple x Nike 50th Anniversary of Hip Hop 2023`,
+     class:`client client-12`,
+    path:`/apple-nike-2023`
+},
+{
+    name:`Alpha Industries`,
+     class:`client client-13`,
+    path:`/alpha`
+},
+{
+    name:`Air Max Day House of Innovation 2023`,
+    class:`client client-14`,
+    path:`/air-max-2023`
+},
+]
--- portfolio/src/Components /Pages/Work.js
+++ portfolio/src/Components /Pages/Work.js
@@ -1,8 +1,24 @@
 import React from 'react'
+import { client_list } from '../Data/Clients';
+import '../../Styles/Work.css'
 
 const Work = () => {
+
   return (
-    <div>Work</div>
+    <div>
+      <h1 className='client-heading'>WORK</h1>
+
+<div className='client-list'>
+{client_list.map((client)=>{
+return(<div className='client-card' >
+<a href={client.path}>
+  <div className={client.class}></div>
+</a>
+<h2>{client.name}</h2>
+</div>)
+})}
+</div>
+    </div>
   )
 }
 
